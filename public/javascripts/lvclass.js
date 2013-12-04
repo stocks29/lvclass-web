041871f17b1d49a72ec3ec1ea9237da736ffe949
Bob Stockdale <robert.stockdale@gmail.com>
2013-12-04 11:13:19 -0500

    More fields

--- public/javascripts/lvclass.js
+++ public/javascripts/lvclass.js
@@ -1,5 +1,11 @@
 angular.module('lvclass', ['ngRoute', 'ngResource'])
 
+.value('eventsUrl', '/api/events')
+
+.factory('Events', function($resource, eventsUrl) {
+    return $resource(eventsUrl);
+})
+
 .config(function($routeProvider) {
     $routeProvider
         .when('/events', {
@@ -11,28 +17,7 @@ angular.module('lvclass', ['ngRoute', 'ngResource'])
         });
 })
 
-.controller('EventListCtrl', function($scope, $resource) {
-    var Events = $resource('/api/events');
-
+.controller('EventListCtrl', function($scope, Events) {
     $scope.events = Events.query();
-
-//    $scope.events = [
-//        {
-//            "dayOfWeek": "Varies",
-//            "_id": 1,
-//            "description": "No description",
-//            "title": "CB Video Fitness",
-//            "dateRange": "2014-01-06T14:00:00",
-//            "mapUrl": "http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=3930+Cambridge+Street+Las+Vegas+NV+89119",
-//            "eventCategory": "ADULT",
-//            "contactNumber": "(702)455-7169",
-//            "fee": "$0",
-//            "location": "Varies",
-//            "address": "3930 Cambridge Street, Las Vegas NV, 89119",
-//            "ageRange": "30 years and Up",
-//            "timeOfDay": "Varies",
-//            "id": "102301-01"
-//        }
-//    ];
 });
 
