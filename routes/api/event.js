07a2f26b90dab072f427823dbc116c6d62521bcf
Bob Stockdale <robert.stockdale@gmail.com>
2013-12-04 16:01:45 -0500

    Add single event details page. Add basic review form.

--- routes/api/event.js
+++ routes/api/event.js
@@ -20,3 +20,21 @@ exports.list = function(req, res){
     });
 
 };
+
+exports.event = function(req, res){
+    var eventId = req.params.eventId;
+    var eventUrl = 'http://demo.ckan.org/api/action/datastore_search?resource_id=acfa8820-38bf-435b-8afb-9c5b9f59013b&filters={"_id":"'
+        + eventId + '"}';
+
+    request(eventUrl, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log("Got response: " + response.statusCode);
+            var result = JSON.parse(body);
+            res.set('Content-Type', 'application/json');
+            res.send(result.result.records[0]);
+        } else {
+            console.log("Error fetching data: " + error);
+            res.send({ "error": "Unable to retrieve data"}, 500);
+        }
+    });
+};
